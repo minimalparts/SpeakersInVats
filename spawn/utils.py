0314ad9c07baaf8acb2b6bbf8459954028b7e646
minimalparts <aurelie.herbelot@cantab.net>
2019-09-18 21:55:05 +0200

    Added spawn directory

---
+++ spawn/utils.py
@@ -0,0 +1,141 @@
+import random
+from sklearn.metrics import pairwise_distances
+from scipy.spatial.distance import cosine
+from sklearn.decomposition import PCA
+from sklearn.decomposition import TruncatedSVD
+from sklearn import preprocessing
+from scipy import linalg as LA
+import numpy as np
+
+
+def get_rand_freq(word_freqs):
+    '''Return a random frequency out of vocabulary'''
+    freq = 0.0
+    while freq == 0.0:
+        freq = word_freqs[random.choice(word_freqs.keys())]
+    return freq
+
+def get_rand_int_vec(dim,max_sum):
+    print(dim,max_sum)
+    r = [random.random() for i in range(dim)]
+    r = [round(max_sum*i/sum(r)) for i in r]
+    return r
+
+def compute_freq_diff(fo,new):
+    fn = new.sum()
+    print("FO",fo, "FN",fn)
+    diff = float(fn) / float(fo)
+    return diff
+
+def get_zero_positions(vec):
+    zeros = []
+    for i in range(len(vec)):
+        if vec[i] == 0:
+            zeros.append(i)
+    return zeros
+
+def fix_vec_freq(vec,diff,up):
+    '''Get vec to correct frequency'''
+    for i in range(diff):
+        position = random.randint(0,len(vec)-1)
+        if up:
+             vec[position]+=1
+        else:
+             while vec[position]==0:			#Super-hack
+                 position+=1
+             vec[position]-=1
+    return vec
+
+def normalise(m):
+    norm_matrix = np.zeros(m.shape)
+    for i in range(m.shape[0]):
+        norm_matrix[i] = m[i] / np.sum(m[i])
+    return norm_matrix
+
+def normalise_l1(m):
+    return preprocessing.normalize(m, norm='l1')
+
+def normalise_l2(m):
+    return preprocessing.normalize(m, norm='l2')
+
+def compute_cosines(m):
+    return 1-pairwise_distances(m, metric="cosine")
+
+def ppmi(m):
+    ppmi_matrix = np.zeros(m.shape)
+    N = np.sum(m)
+    row_sums = np.sum(m, axis=1)
+    for i in range(m.shape[0]):
+        for j in range(m.shape[1]):
+            ppmi_matrix[i][j] = max(0, m[i][j] * N / (row_sums[i] * row_sums[j]))
+    return ppmi_matrix
+
+def compute_PCA(m,dim):
+    m -= np.mean(m, axis = 0)
+    pca = PCA(n_components=dim)
+    pca.fit(m)
+    return pca.transform(m)
+
+def compute_truncated_SVD(m,dim):
+    m -= np.mean(m, axis = 0)
+    tsvd = TruncatedSVD(dim)
+    tsvd.fit(m) 
+    return tsvd.transform(m)
+
+def read_external_vectors(vector_file):
+    vocab = []
+    vectors = []
+    with open(vector_file) as f:
+        dmlines=f.read().splitlines()
+    for l in dmlines:
+        items=l.split()
+        target = items[0]
+        vocab.append(target)
+        vec=[float(i) for i in items[1:]]       #list of lists  
+        vectors.append(vec)
+    m = np.array(vectors)
+    return m, vocab
+
+def read_reference_vectors(vector_file):
+    word_freqs = {}
+    word_vars = {}
+    vocab = []
+    vectors = []
+    with open(vector_file) as f:
+        dmlines=f.read().splitlines()
+    for l in dmlines:
+        items=l.split()
+        target = items[0]
+        vocab.append(target)
+        vec = np.array([float(i) for i in items[1:]])
+        word_freqs[target] = int(vec.sum())
+        word_vars[target] = int(round(np.var(vec)))
+        vectors.append(vec)
+        if np.sum(vec) == 0:
+           print("WARNING",target,"HAS 0 SUM")
+    m = np.array(vectors)
+    return vocab, m, word_freqs, word_vars
+
+
+def print_matrix(dm_mat,vocab,outfile):
+    '''Print new dm file'''
+    f = open(outfile,'w')
+    for c in range(dm_mat.shape[0]):
+        vec = ' '.join([str(i) for i in dm_mat[c]])
+        f.write(vocab[c]+" "+vec+"\n")
+    f.close()
+
+def print_vocab(vocab,outfile):
+    '''Print new vocab file'''
+    f = open(outfile,'w')
+    for c in range(len(vocab)):
+        line = str(c)+' '+vocab[c]+'\n'
+        f.write(line)
+    f.close()
+
+def print_dict(d,outfile):
+    f = open(outfile,'w')
+    for k,v in d.items():
+        line = str(k)+' '+str(v)+'\n'
+        f.write(line)
+    f.close()
---
+++ spawn/vat.py
@@ -0,0 +1,109 @@
+"""Create speakers by adding noise to a reference matrix
+
+Usage:
+vat.py <file_in> <dimensionality> <num_speakers>
+vat.py --version
+
+Options:
+-h --help     Show this screen.
+--version     Show version.
+"""
+
+import os
+import MEN
+import sys
+import random
+import numpy as np
+from utils import read_external_vectors,ppmi,normalise_l2,compute_PCA,print_matrix, print_vocab, print_dict
+from docopt import docopt
+import shutil
+
+
+def mk_vat(args,vocab,speakers,vat_dir):
+    num_files_in_dir = len([name for name in os.listdir(vat_dir)])
+    dir_num = num_files_in_dir+1
+    new_dir = "vat"+str(dir_num)
+    os.makedirs(vat_dir+'/'+new_dir)
+
+    for c in range(len(speakers)):
+        print_matrix(speakers[c],vocab,vat_dir+'/'+new_dir+'/s'+str(c)+'.dm')
+
+    print_vocab(vocab,vat_dir+'/'+new_dir+'/vocab.txt')
+    print_dict(args,vat_dir+'/'+new_dir+'/settings.txt')
+    shutil.make_archive(vat_dir+'/'+new_dir,'zip',vat_dir,new_dir)
+    shutil.rmtree(vat_dir)
+    
+
+def get_realistic_perturbation(original,frozen):
+    new_vec = original.copy()
+    perc = random.randint(95,105)
+    add = np.random.choice([0,1],p=[0.99,0.01])
+    for i in range(len(new_vec)):
+        if i not in frozen:
+            new_vec[i] = int(perc * original[i] / 100)
+            new_vec[i] = max(0,new_vec[i]+add)
+    return np.array(new_vec)
+
+
+def reset_word(dm_mat,i,new_random):
+    dm_mat[i] = new_random
+    dm_mat = dm_mat.T
+    dm_mat[i] = new_random
+    dm_mat = dm_mat.T
+    return dm_mat
+
+def process_matrix(m,dim):
+    m = ppmi(m)
+    m = normalise_l2(m)
+    m = compute_PCA(m,dim)
+    return m
+
+def make_speaker(m,vocab,dim):
+    frozen= []
+    ref_speaker = np.copy(m)
+    test_speaker = np.copy(m)
+    for i in range(test_speaker.shape[0]):
+        original = test_speaker[i]
+        new_random = get_realistic_perturbation(original,frozen)
+        test_speaker = reset_word(test_speaker,i,new_random)
+        frozen.append(i)
+    
+    ref_speaker_proc = process_matrix(ref_speaker,dim)
+    test_speaker_proc = process_matrix(test_speaker,dim)
+
+    between_speakers = 0.0
+    sp1_men = MEN.compute_spearman(ref_speaker_proc,vocab)[0]
+    sp2_men = MEN.compute_spearman(test_speaker_proc,vocab)[0]
+    return test_speaker,between_speakers,sp1_men,sp2_men
+
+
+if __name__=="__main__":
+    args = docopt(__doc__, version='Speakers in vats, noise 0.1')
+    m, vocab = read_external_vectors(args["<file_in>"])
+    dimensionality = int(args["<dimensionality>"])
+
+    vat_dir = "./allvats"
+    if not os.path.exists(vat_dir):
+        os.makedirs(vat_dir)
+
+    speakers=[]
+    all_between_speakers = []
+    all_sp1_men = []
+    all_sp2_men = []
+
+
+
+    for i in range(int(args["<num_speakers>"])):
+        ref_speaker = np.copy(m)
+        test_speaker, between_speakers, sp1_men, sp2_men = make_speaker(ref_speaker,vocab,dimensionality)
+        all_between_speakers.append(between_speakers)
+        all_sp1_men.append(sp1_men)
+        all_sp2_men.append(sp2_men)
+
+        print("SPEARMAN REF/TEST",between_speakers)
+        print("SPEARMAN REF",sp1_men)
+        print("SPEARMAN TEST",sp2_men)
+        speakers.append(test_speaker)
+        
+
+    mk_vat(args,vocab,speakers,vat_dir)
